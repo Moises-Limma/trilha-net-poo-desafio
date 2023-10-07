33b0a57861235825be97ba4bc04be65c71391587
Moises Lima <moises.pl@hotmail.com>
2023-10-07 20:46:56 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using DesafioPOO.Models;
+
+Smartphone Nokia = new Nokia(numero: "111111", modelo: "Nokia", imei: "111111111", memoria: 6);
+Console.WriteLine("Smartphone Nokia:");
+Nokia.Ligar();
+Nokia.InstalarAplicativo("DIO");
+
+Console.WriteLine("\n");
+
+Smartphone Iphone = new Iphone(numero: "222222", modelo: "Iphone", imei: "222222222", memoria: 8);
+Console.WriteLine("Smartphone Iphone:");
+Iphone.ReceberLigacao();
+Iphone.InstalarAplicativo("IDE Code");
+
+
